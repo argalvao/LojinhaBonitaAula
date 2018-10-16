7c7a070142c5e65050bc1351c65c911b944e9f4a
Mayara <mayaradiasg@gmail.com>
2018-10-16 17:52:54 -0300

    Initial commit

---
+++ resources/views/categorias.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.base', ["current"=>"categorias"])
+
+@section('body')
+    <h1>Categorias</h1>
+@endsection
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.base', ["current"=>"produtos"])
+
+@section('body')
+    <h1>Produtos</h1>
+@endsection
